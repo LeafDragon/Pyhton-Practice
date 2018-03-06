2e3ba612c392cace7a3c0147474b03e779c0e65b
Frank Dip <33531671+LeafDragon@users.noreply.github.com>
2018-03-05 17:50:48 -0800

    Even or Odd print out

---
+++ MITx 6.00.1x Intro CS/evenOdd.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+created on 2018-03-05
+
+@author: Frank Dip
+"""
+
+x = int(input(""Enter an integer: "))
+if x % 2 == 0:
+    print("Your number is even")
+else:
+    print("Your number is odd")
+print("End!")
