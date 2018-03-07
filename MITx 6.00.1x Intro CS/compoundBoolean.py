ccec0a0c69606b3301b31110cf456eced2b862d0
Frank Dip <33531671+LeafDragon@users.noreply.github.com>
2018-03-06 17:45:48 -0800

    Compound Boolean

---
+++ MITx 6.00.1x Intro CS/compoundBoolean.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2018-03-06
+
+@author: Frank Dip
+"""
+
+x = 6
+y = 7
+z = 5
+
+if x < y and x < z:
+    print("x is least")
+elif y < z:
+    print("y is least")
+else:
+    print("z is least")
