930186ea845fd0426cb118a7c4883a9f5465f8f9
Frank Dip <33531671+LeafDragon@users.noreply.github.com>
2018-03-09 12:47:28 -0800

    Print example with num and str concatenation

---
+++ MITx 6.00.1x Intro CS/printExample.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  9 12:28:39 2018
+
+@author: Frank Dip
+"""
+
+x = 1
+print(x)
+
+x_str = str(x)
+
+print("My favorite number is", x, ".", "x=", x)
+print("My favorite number is " + x_str + ". x=" + x_str)
